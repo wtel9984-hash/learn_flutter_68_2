5acd085d7f2cb628c4cf22e9c6ac0507239faf93
Thalatchanan <wtel9984@gmail.com>
2026-06-13 10:45:20 +0700

    Step 6 : Image widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,54 +5,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        // return Row(
     return Column(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: Colors.tealAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text( 
-            "Hello Flutter",
-            style: TextStyle( 
-              fontSize: 20,
-              color: Colors.pinkAccent,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-          Container(
-          color: const Color.fromARGB(255, 0, 248, 99),
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text( 
-            "Hello Flutter",
-            style: TextStyle( 
-              fontSize: 20,
-              color: const Color.fromARGB(255, 66, 92, 165),
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: const Color.fromARGB(255, 125, 255, 3),
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text( 
-            "Hello Flutter",
-            style: TextStyle( 
-              fontSize: 20,
-              color: Colors.pinkAccent,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
+        Image.asset("assets/images/cat1.jpg", width: 200, height: 200),
+        SizedBox(height: 20),
+        Image.network(
+          "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg",
+          width: 200,
+          height: 200,
+        )
       ],
     );
   }
