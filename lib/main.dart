082233eb0d0fcf248a9abdfd698f6090de11f764
Thalatchanan <wtel9984@gmail.com>
2026-06-20 13:49:35 +0700

    Step 15 : Form state,Step 16 : Sumit

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:learn_flutter_68_2/screen/home.dart';
 
 import 'package:learn_flutter_68_2/screen/item.dart';
+
+import 'package:learn_flutter_68_2/screen/addFrom.dart';
 void main() {
   // runApp(const MyApp());
 
   // const app = MaterialApp(title : "หัวข้อ", home: Text("Hello World"));
   // runApp(app);
 
-  runApp(
-    MaterialApp(
-      title: "หัวข้อ",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("หัวข้อ"),
-          backgroundColor: Colors.indigoAccent,
-          centerTitle: true,
-        ),
-        body: Item(),
-     ),
-    ),
-  );
+  // runApp(
+  //   MaterialApp(
+  //     title: "หัวข้อ",
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text("หัวข้อ"),
+  //         backgroundColor: Colors.indigoAccent,
+  //         centerTitle: true,
+  //       ),
+  //       body: Item(),
+  //    ),
+  //   ),
+  // );
+  runApp(AddForm());
 }
 
--- lib/screen/addFrom.dart
+++ lib/screen/addFrom.dart
@@ -10,6 +10,11 @@ class AddForm extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<AddForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+  int _age = 20;
+  Job _job = Job.developer;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,29 +27,116 @@ class _MyWidgetState extends State<AddForm> {
         ),
         body: Padding(
           padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              TextFormField(decoration: InputDecoration(labelText: "Name")),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Age"),
-                keyboardType: TextInputType.number,
-              ),
-              DropdownButtonFormField<Job>(
-                decoration: const InputDecoration(labelText: "Job"),
-                items: Job.values.map((job) {
-                  return DropdownMenuItem<Job>(
-                    value: job,
-                    child: Text(job.title),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  print("Selected Job : ${value?.title}");
-                },
-              ),
-            ],
+          // child: Column(
+          //   children: [
+          //     TextFormField(decoration: InputDecoration(labelText: "Name")),
+          //     TextFormField(
+          //       decoration: InputDecoration(labelText: "Age"),
+          //       keyboardType: TextInputType.number,
+          //     ),
+          //     DropdownButtonFormField<Job>(
+          //       decoration: const InputDecoration(labelText: "Job"),
+          //       items: Job.values.map((job) {
+          //         return DropdownMenuItem<Job>(
+          //           value: job,
+          //           child: Text(job.title),
+          //         );
+          //       }).toList(),
+          //       onChanged: (value) {
+          //         print("Selected Job : ${value?.title}");
+          //       },
+          //     ),
+          //     SizedBox(height: 20),
+          //     FilledButton(
+          //       onPressed:  (){
+
+          //       },
+          //       style: FilledButton.styleFrom(
+          //         backgroundColor: Colors.blue,
+          //         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+          //       ),
+          //       child: Text(
+          //         "Summit",
+          //         style: TextStyle(color: Colors.deepOrange, fontSize: 20),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Name"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "please enter a name";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value ?? "";
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Age"),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'please enter age';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'please enter a valid number';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _age = int.tryParse(value ?? '') ?? 20;
+                  },
+                ),
+                DropdownButtonFormField<Job>(
+                  decoration: const InputDecoration(labelText: "Job"),
+                  items: Job.values.map((job) {
+                    return DropdownMenuItem<Job>(
+                      value: job,
+                      child: Text(job.title),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _job = value!;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      _formKey.currentState?.save();
+                      print("Name : $_name , Age : $_age , Job : ${_job?.title}");
+
+                      setState(() {
+                        people.add(Person(name: _name, age: _age, job: _job));
+                      });
+
+                      _formKey.currentState?.reset();
+                    }
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.black87,
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50), 
+                  ),
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(color: Colors.deepOrangeAccent , fontSize: 20),
+                  ),
+                )
+              ],
+            ),
           ),
-        )
-        )
-      );
+        ),
+      ),
+    );
   }
 }
